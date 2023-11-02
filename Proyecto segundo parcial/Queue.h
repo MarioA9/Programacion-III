a33d9b0ef38edc998484dcb9c151232b230c020b
MarioA9 <mario.rivera@uniat.edu.mx>
2023-11-02 01:47:28 -0500

    correccion segundo resumen

---
+++ Proyecto segundo parcial/Queue.h
@@ -0,0 +1,45 @@
+#ifndef PROYECTO_SEGUNDO_PARCIAL_QUEUE_H
+#define PROYECTO_SEGUNDO_PARCIAL_QUEUE_H
+// enqueue copia referencia r y l
+// dequeue
+//front
+// print
+
+template<typename ZL>
+class Queue : private DLList<ZL>{
+public:
+    Queue(){
+    }
+
+    ~Queue(){
+        clear();
+        delete DLList<ZL>::head;
+        delete DLList<ZL>::tail;
+    }
+
+    void enqueue(ZL &data){
+        DLList<ZL>::push_back(data);
+    }
+
+    void enqueue(ZL &&data){
+        DLList<ZL>::push_back(data);
+    }
+
+    void dequeue(){
+        DLList<ZL>::pop_front();
+    }
+
+    void print(){
+        DLList<ZL>::print();
+    }
+
+    void clear(){
+        DLList<ZL>::clear();
+    }
+
+    ZL front(){
+        return DLList<ZL>::head->next->data;
+    }
+};
+
+#endif //PROYECTO_SEGUNDO_PARCIAL_QUEUE_H
