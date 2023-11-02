ef00c80a962d114ad229c540bdc20050b9a144ca
MarioA9 <mario.rivera@uniat.edu.mx>
2023-11-02 10:36:45 -0500

    proyecto finalizado

--- Proyecto segundo parcial/Queue.h
+++ Proyecto segundo parcial/Queue.h
@@ -6,38 +6,38 @@
 // print
 
 template<typename ZL>
-class Queue : private DLList<ZL>{
+class Queue : private DLList<ZL>{ // se crea una clase para queu y hereda de DLList
 public:
-    Queue(){
+    Queue(){ // se crea el constructor simple
     }
 
-    ~Queue(){
+    ~Queue(){ // se crea el destructor
         clear();
-        delete DLList<ZL>::head;
-        delete DLList<ZL>::tail;
+        delete DLList<ZL>::head; // se borra el head heredado del DLList
+        delete DLList<ZL>::tail; // se borra el tail heredado del DLList
     }
 
-    void enqueue(ZL &data){
+    void enqueue(ZL &data){ // inserta el dato al final del queu
         DLList<ZL>::push_back(data);
     }
 
-    void enqueue(ZL &&data){
+    void enqueue(ZL &&data){ // inserta por referencia el dato al final del queu
         DLList<ZL>::push_back(data);
     }
 
-    void dequeue(){
+    void dequeue(){  // borra el dato al inicio de la lista
         DLList<ZL>::pop_front();
     }
 
-    void print(){
+    void print(){ // se imprime todo
         DLList<ZL>::print();
     }
 
-    void clear(){
+    void clear(){ // borra toda la lista
         DLList<ZL>::clear();
     }
 
-    ZL front(){
+    ZL front(){ // imprime solamente el primero
         return DLList<ZL>::head->next->data;
     }
 };
--- Proyecto segundo parcial/Stack.h
+++ Proyecto segundo parcial/Stack.h
@@ -8,37 +8,37 @@
 #define PROYECTO_SEGUNDO_PARCIAL_STACK_H
 
 template <typename Iterator>
-        class Stack : private DLList<Iterator> {
+        class Stack : private DLList<Iterator> { // se crea una clase para stack y hereda de DLList
         public:
-            Stack() {
+            Stack() { // se crea el constructor simple
             }
 
-            ~Stack(){
+            ~Stack(){ // se crea el destructor
                 clear();
                 delete DLList<Iterator>::head;
                 delete DLList<Iterator>::tail;
             }
 
-            void push(Iterator &data){
+            void push(Iterator &data){ // inserta el dato al inicio del stack
                 DLList<Iterator>::push_front(data);
             }
-            void push(Iterator &&data){
+            void push(Iterator &&data){  // inserta por referencia el dato al inicio del Stack
                 DLList<Iterator>::push_front(data);
             }
 
-            void pop(){
+            void pop(){  // borra el dato al inicio de la lista
                 DLList<Iterator>::pop_front();
             }
 
-            void clear(){
+            void clear(){ // borra toda la lista
                 DLList<Iterator>::clear();
             }
 
-            void print(){
+            void print(){ // se imprime todo
                 DLList<Iterator>::print();
             }
 
-            Iterator top(){
+            Iterator top(){ // imprime solamente el primero
                 return DLList<Iterator>::head->next->data;
             }
         };
